6fcc615d0719912b34aacb4e673b90e4bb56c2e2
Abizzar <168212270+AbizzarG@users.noreply.github.com>
2024-08-25 02:46:49 +0700

    ReInstalled Directory, Added Home Page, Added Components
    Yang Baru dikerjain hanya Homepage; Komponen yang ditambah ada FoodCard, Navigaton Bar;Home Page Masih Belum sesuai dengan template; Masih Banyak Bug didalam HomePage; Beberapa Page lain sudah ditambahkan namun belum di kerjakan; Masih banyak Page yang belum ditambahkan;Jika ada pertanyaan tolong tanyakan di grup

---
+++ itbites-13/app/MenuPage/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenusPage from '@/components/MenusPage'
+import NavBar from '@/components/NavBar'
+
+const MenuPage = () => {
+  return (
+    <div>
+        <NavBar/>
+        <MenusPage/> 
+    </div>
+  )
+}
+
+export default MenuPage
---
+++ itbites-13/components/FoodCard.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+import { MENU } from "../public/assets/constant";
+
+//sebelum pake harus instal font terlebih dahulu di global css dan juga di tailwind config, terus harus import gambarnya yang sesuai
+
+const FoodCard = () => {
+
+  const menu = MENU[0];
+  return (
+<div className='flex flex-col py-0 border border-[#d3773b] border-opacity-50 rounded-[20px] shadow-2xl p-0 m-0 h-[450px]'>
+        <Image src={menu.image} alt={menu.menuname} className=' w-[300px] h-[300px] cover rounded-t-[20px]'/>
+        <div className='bg-gradient-to-t from-[#fe9b5b] to-[#fe6601] w-[300px] h-[150px] rounded-b-[20px] font-gilroy'  > 
+            <h3 className='flex items-start justify-start text-4xl font-extrabold text-white pt-8 pl-8'>{menu.menuname}</h3>
+            <p className='text-white flex justify-start items-start pl-8 text-2xl font-bold pt-2'>{menu.price}</p>
+        </div>
+    </div>
+        
+  )
+}   
+
+export default FoodCard;
