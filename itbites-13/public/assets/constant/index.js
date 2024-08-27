06c772f7863107915a2c964e511eb89f131e3d92
jandhiesto <163627504+jandhiesto@users.noreply.github.com>
2024-08-28 06:49:35 +0700

    chore: updated data files for indexing

--- itbites-13/public/assets/constant/index.js
+++ itbites-13/public/assets/constant/index.js
@@ -4,8 +4,64 @@ import menu3 from "../../assets/Placeholder/Cromboloni.jpeg";
 import menu4 from "../../assets/Placeholder/Gyoza.jpeg";
 import menu5 from "../../assets/Placeholder/Mie Kuah.jpg";
 import menu6 from "../../assets/Placeholder/NasiGoreng.jpeg";
-import menu7 from "../../assets/Placeholder/Soto Padang.jpeg";
+import menu7 from "../../assets/Placeholder/SotoPadang.jpeg";
 import menu8 from "../../assets/Placeholder/Sushi.jpg";
+import menu9 from "../../assets/Placeholder/GadoGado.jpg";
+import menu10 from "../../assets/Placeholder/KeripikSingkong.jpeg";
+import menu11 from "../../assets/Placeholder/AyamGoreng.jpeg";
+import menu12 from "../../assets/Placeholder/TehTarik.jpg";
+import menu13 from "../../assets/Placeholder/BaksoRaksasa.jpeg";
+import menu14 from "../../assets/Placeholder/Klepon.jpeg";
+import menu15 from "../../assets/Placeholder/NasiUduk.jpeg";
+import menu16 from "../../assets/Placeholder/EsCampur.jpeg";
+import menu17 from "../../assets/Placeholder/NasiAyamGeprek.jpeg";
+import menu18 from "../../assets/Placeholder/PisangGoreng.jpeg";
+import menu19 from "../../assets/Placeholder/JusAlpukat.jpeg";
+import menu20 from "../../assets/Placeholder/SopBuntut.jpeg";
+import menu21 from "../../assets/Placeholder/Batagor.jpeg";
+import menu22 from "../../assets/Placeholder/MieRebus.jpeg";
+import menu23 from "../../assets/Placeholder/SusuCoklat.jpeg";
+import menu24 from "../../assets/Placeholder/LontongSayur.jpeg";
+import menu25 from "../../assets/Placeholder/TahuIsi.jpeg";
+import menu26 from "../../assets/Placeholder/NasiKuning.jpeg";
+import menu27 from "../../assets/Placeholder/EsDoger.jpeg";
+import menu28 from "../../assets/Placeholder/Rujak.jpeg";
+import menu29 from "../../assets/Placeholder/SateKambing.jpeg";
+import menu30 from "../../assets/Placeholder/BuburAyam.jpeg";
+import menu31 from "../../assets/Placeholder/Donat.jpeg";
+import menu32 from "../../assets/Placeholder/KopiHitam.jpeg";
+import menu33 from "../../assets/Placeholder/NasiPecel.jpeg";
+import menu34 from "../../assets/Placeholder/SingkongRebus.jpeg";
+import menu35 from "../../assets/Placeholder/EsKacangHijau.jpeg";
+import menu36 from "../../assets/Placeholder/IkanBakar.jpeg";
+import menu37 from "../../assets/Placeholder/MartabakManis.jpeg";
+import menu38 from "../../assets/Placeholder/EsBuah.jpeg";
+import menu39 from "../../assets/Placeholder/NasiRawon.jpeg";
+import menu40 from "../../assets/Placeholder/KueCubit.jpeg";
+import menu41 from "../../assets/Placeholder/JusMangga.jpeg";
+import menu42 from "../../assets/Placeholder/AyamBakar.jpeg";
+import menu43 from "../../assets/Placeholder/KacangRebus.jpeg";
+import menu44 from "../../assets/Placeholder/WedangJahe.jpeg";
+import menu45 from "../../assets/Placeholder/SotoDaging.jpeg";
+import menu46 from "../../assets/Placeholder/Cireng.jpeg";
+import menu47 from "../../assets/Placeholder/SusuJahe.jpeg";
+import menu48 from "../../assets/Placeholder/NasiLiwet.jpeg";
+import menu49 from "../../assets/Placeholder/DonatKentang.jpeg";
+import menu50 from "../../assets/Placeholder/EsCincau.jpeg";
+import menu51 from "../../assets/Placeholder/MieGoreng.jpeg";
+import menu52 from "../../assets/Placeholder/TahuSumedang.jpeg";
+import menu53 from "../../assets/Placeholder/SopIga.jpeg";
+import menu54 from "../../assets/Placeholder/Kerupuk.jpeg";
+import menu55 from "../../assets/Placeholder/EsJerukKelapa.jpeg";
+import menu56 from "../../assets/Placeholder/NasiBebek.jpeg";
+import menu57 from "../../assets/Placeholder/OndeOnde.jpeg";
+import menu58 from "../../assets/Placeholder/EsPisangIjo.jpeg";
+import menu59 from "../../assets/Placeholder/NasiGudeg.jpeg";
+import menu60 from "../../assets/Placeholder/PisangCoklat.jpeg";
+import menu61 from "../../assets/Placeholder/Arsik.jpeg";
+import menu62 from "../../assets/Placeholder/SateAyam.jpeg";
+import menu63 from "../../assets/Placeholder/EsTehManis.jpeg";
+import menu64 from "../../assets/Placeholder/NasiPadang.jpeg";
 import K_EC1 from "../../assets/East Corner/Kantin_QnQ.jpg"
 import K_EC2 from "../../assets/East Corner/Kantin_Matahari.jpg"
 import K_GKUB1 from "../../assets/GKUB/Raflesia.jpg"
@@ -89,6 +145,510 @@ export const MENU = [
         link: "/MakananDetailPage/Sushi",
         jenis: "Makanan Berat"
     },
+    {
+        image: menu9,
+        menuname: "Gado-Gado",
+        price: "Rp15.000",
+        location: "GKU 2",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/GadoGado",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu10,
+        menuname: "Keripik Singkong",
+        price: "Rp5.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/KeripikSingkong",
+        jenis: "Camilan"
+    },
+    {
+        image: menu11,
+        menuname: "Ayam Goreng",
+        price: "Rp20.000",
+        location: "GKU Barrat",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/AyamGoreng",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu12,
+        menuname: "Teh Tarik",
+        price: "Rp8.000",
+        location: "Kehutanan",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/TehTarik",
+        jenis: "Minuman"
+    },
+    {
+        image: menu13,
+        menuname: "Bakso Raksasa",
+        price: "Rp20.000",
+        location: "East Corner",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/BaksoRaksasa",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu14,
+        menuname: "Klepon",
+        price: "Rp7.000",
+        location: "GKU T 2",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/Klepon",
+        jenis: "Camilan"
+    },
+    {
+        image: menu15,
+        menuname: "Nasi Uduk",
+        price: "Rp15.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/NasiUduk",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu16,
+        menuname: "Es Campur",
+        price: "Rp10.000",
+        location: "GKU 2",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/EsCampur",
+        jenis: "Minuman"
+    },
+    {
+        image: menu17,
+        menuname: "Nasi Ayam Geprek",
+        price: "Rp18.000",
+        location: "KOICA",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/NasiAyamGeprek",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu18,
+        menuname: "Pisang Goreng",
+        price: "Rp5.000",
+        location: "GKU T 1",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/PisangGoreng",
+        jenis: "Camilan"
+    },
+    {
+        image: menu19,
+        menuname: "Jus Alpukat",
+        price: "Rp12.000",
+        location: "GKU Barrat",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/JusAlpukat",
+        jenis: "Minuman"
+    },
+    {
+        image: menu20,
+        menuname: "Sop Buntut",
+        price: "Rp30.000",
+        location: "East Corner",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/SopBuntut",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu21,
+        menuname: "Batagor",
+        price: "Rp10.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/Batagor",
+        jenis: "Camilan"
+    },
+    {
+        image: menu22,
+        menuname: "Mie Rebus",
+        price: "Rp12.000",
+        location: "GKU 2",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/MieRebus",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu23,
+        menuname: "Susu Coklat",
+        price: "Rp7.000",
+        location: "GKU T 2",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/SusuCoklat",
+        jenis: "Minuman"
+    },
+    {
+        image: menu24,
+        menuname: "Lontong Sayur",
+        price: "Rp12.000",
+        location: "KOICA",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/LontongSayur",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu25,
+        menuname: "Tahu Isi",
+        price: "Rp6.000",
+        location: "Kehutanan",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/TahuIsi",
+        jenis: "Camilan"
+    },
+    {
+        image: menu26,
+        menuname: "Nasi Kuning",
+        price: "Rp15.000",
+        location: "GKU T 1",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/NasiKuning",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu27,
+        menuname: "Es Doger",
+        price: "Rp10.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/EsDoger",
+        jenis: "Minuman"
+    },
+    {
+        image: menu28,
+        menuname: "Rujak",
+        price: "Rp8.000",
+        location: "GKU Barrat",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/Rujak",
+        jenis: "Camilan"
+    },
+    {
+        image: menu29,
+        menuname: "Sate Kambing",
+        price: "Rp30.000",
+        location: "East Corner",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/SateKambing",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu30,
+        menuname: "Bubur Ayam",
+        price: "Rp10.000",
+        location: "GKU T 2",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/BuburAyam",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu31,
+        menuname: "Donat",
+        price: "Rp5.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/Donat",
+        jenis: "Camilan"
+    },
+    {
+        image: menu32,
+        menuname: "Kopi Hitam",
+        price: "Rp5.000",
+        location: "Kehutanan",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/KopiHitam",
+        jenis: "Minuman"
+    },
+    {
+        image: menu33,
+        menuname: "Nasi Pecel",
+        price: "Rp12.000",
+        location: "KOICA",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/NasiPecel",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu34,
+        menuname: "Singkong Rebus",
+        price: "Rp5.000",
+        location: "GKU T 1",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/SingkongRebus",
+        jenis: "Camilan"
+    },
+    {
+        image: menu35,
+        menuname: "Es Kacang Hijau",
+        price: "Rp8.000",
+        location: "GKU Barrat",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/EsKacangHijau",
+        jenis: "Minuman"
+    },
+    {
+        image: menu36,
+        menuname: "Ikan Bakar",
+        price: "Rp25.000",
+        location: "GKU 2",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/IkanBakar",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu37,
+        menuname: "Martabak Manis",
+        price: "Rp20.000",
+        location: "East Corner",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/MartabakManis",
+        jenis: "Camilan"
+    },
+    {
+        image: menu38,
+        menuname: "Es Buah",
+        price: "Rp12.000",
+        location: "GKU T 2",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/EsBuah",
+        jenis: "Minuman"
+    },
+    {
+        image: menu39,
+        menuname: "Nasi Rawon",
+        price: "Rp20.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/NasiRawon",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu40,
+        menuname: "Kue Cubit",
+        price: "Rp10.000",
+        location: "GKU Barrat",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/KueCubit",
+        jenis: "Camilan"
+    },
+    {
+        image: menu41,
+        menuname: "Jus Mangga",
+        price: "Rp10.000",
+        location: "Kehutanan",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/JusMangga",
+        jenis: "Minuman"
+    },
+    {
+        image: menu42,
+        menuname: "Ayam Bakar",
+        price: "Rp22.000",
+        location: "KOICA",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/AyamBakar",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu43,
+        menuname: "Kacang Rebus",
+        price: "Rp5.000",
+        location: "GKU T 1",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/KacangRebus",
+        jenis: "Camilan"
+    },
+    {
+        image: menu44,
+        menuname: "Wedang Jahe",
+        price: "Rp8.000",
+        location: "GKU T 2",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/WedangJahe",
+        jenis: "Minuman"
+    },
+    {
+        image: menu45,
+        menuname: "Soto Daging",
+        price: "Rp20.000",
+        location: "East Corner",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/SotoDaging",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu46,
+        menuname: "Cireng",
+        price: "Rp6.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/Cireng",
+        jenis: "Camilan"
+    },
+    {
+        image: menu47,
+        menuname: "Susu Jahe",
+        price: "Rp8.000",
+        location: "GKU Barrat",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/SusuJahe",
+        jenis: "Minuman"
+    },
+    {
+        image: menu48,
+        menuname: "Nasi Liwet",
+        price: "Rp20.000",
+        location: "Kehutanan",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/NasiLiwet",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu49,
+        menuname: "Donat Kentang",
+        price: "Rp7.000",
+        location: "GKU 2",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/DonatKentang",
+        jenis: "Camilan"
+    },
+    {
+        image: menu50,
+        menuname: "Es Cincau",
+        price: "Rp8.000",
+        location: "KOICA",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/EsCincau",
+        jenis: "Minuman"
+    },
+    {
+        image: menu51,
+        menuname: "Mie Goreng",
+        price: "Rp15.000",
+        location: "GKU T 1",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/MieGoreng",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu52,
+        menuname: "Tahu Sumedang",
+        price: "Rp6.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/TahuSumedang",
+        jenis: "Camilan"
+    },
+    {
+        image: menu53,
+        menuname: "Sop Iga",
+        price: "Rp25.000",
+        location: "East Corner",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/SopIga",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu54,
+        menuname: "Kerupuk",
+        price: "Rp2.000",
+        location: "GKU Barrat",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/Kerupuk",
+        jenis: "Camilan"
+    },
+    {
+        image: menu55,
+        menuname: "Es Jeruk Kelapa",
+        price: "Rp12.000",
+        location: "GKU T 2",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/EsJerukKelapa",
+        jenis: "Minuman"
+    },
+    {
+        image: menu56,
+        menuname: "Nasi Bebek",
+        price: "Rp25.000",
+        location: "Kehutanan",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/NasiBebek",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu57,
+        menuname: "Onde-Onde",
+        price: "Rp6.000",
+        location: "GKU 2",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/OndeOnde",
+        jenis: "Camilan"
+    },
+    {
+        image: menu58,
+        menuname: "Es Pisang Ijo",
+        price: "Rp10.000",
+        location: "KOICA",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/EsPisangIjo",
+        jenis: "Minuman"
+    },
+    {
+        image: menu59,
+        menuname: "Nasi Gudeg",
+        price: "Rp20.000",
+        location: "GKU T 1",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/NasiGudeg",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu60,
+        menuname: "Pisang Coklat",
+        price: "Rp6.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/PisangCoklat",
+        jenis: "Camilan"
+    },
+    {
+        image: menu61,
+        menuname: "Arsik",
+        price: "Rp25.000",
+        location: "GKU T 1",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/Arsik",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu62,
+        menuname: "Sate Ayam",
+        price: "Rp20.000",
+        location: "Barrac",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/SateAyam",
+        jenis: "Makanan Berat"
+    },
+    {
+        image: menu63,
+        menuname: "Es Teh Manis",
+        price: "Rp5.000",
+        location: "GKU Barrat",
+        kampus: "Ganesha",
+        link: "/MakananDetailPage/EsTehManis",
+        jenis: "Minuman"
+    },
+    {
+        image: menu64,
+        menuname: "Nasi Padang",
+        price: "Rp22.000",
+        location: "Kehutanan",
+        kampus: "Jatinangor",
+        link: "/MakananDetailPage/NasiPadang",
+        jenis: "Makanan Berat"
+    }
 ];
 
 export const KANTIN = [
