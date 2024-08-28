f0b7f20e3431c8b205d2145f07a4a78abb9491b1
Starath <19623055@std.stei.itb.ac.id>
2024-08-28 11:29:11 +0700

    chore : changed <a> to <Link>

--- itbites-13/components/NavBar.js
+++ itbites-13/components/NavBar.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Foto from "../public/assets/itbiteslogowhite.png";
 import {  RiAccountCircleFill  } from "react-icons/ri";
 import Link from "next/link";
+
 const NavBar = () => {
   return (
     <div>
@@ -10,12 +11,12 @@ const NavBar = () => {
         <div className="flex items-center space-x-4">
           <Link href='../'><Image src={Foto} alt="logo" width={130} className='ml-12' /></Link>
           <div className='flex space-x-4'>
-            <a href="..\KantinPage" className="text-white font-bold mx-36 text-3xl">
+            <Link href="..\KantinPage" className="text-white font-bold mx-36 text-3xl">
               kantin
-            </a>
-            <a href="..\MakananPage" className="text-white font-bold text-3xl">
+            </Link>
+            <Link href="..\MakananPage" className="text-white font-bold text-3xl">
               makanan
-            </a>
+            </Link>
           </div>
         </div>
         <a href='#'><RiAccountCircleFill className='w-14 h-14 right-0' color='white'/></a>
